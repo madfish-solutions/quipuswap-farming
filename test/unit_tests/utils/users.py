dc7aace6d71d8392cb4acdfdd4ea15c482bb360a
smak0v <makov.sergey.it@gmail.com>
2021-08-09 12:18:37 +0300

    Reset tests

--- test/unit_tests/utils/users.py
+++
@@ -1,6 +0,0 @@
-from pytezos import pytezos
-
-admin = pytezos.key.public_key_hash()
-alice = 'tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb'
-bob = 'tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6'
-zero = 'tz1ZZZZZZZZZZZZZZZZZZZZZZZZZZZZNkiRg'

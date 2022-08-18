3a244624fedf5053c210fc5093d9bc1f5071cbf3
cyace84 <cyace84@gmail.com>
2022-08-18 12:47:59 +0200

    fixed Tfarm4 tests

--- test/TFarm4.spec.ts
+++ test/TFarm4.spec.ts
@@ -4,9 +4,7 @@ import { Utils, zeroAddress } from "./helpers/Utils";
 import { TFarm, TFarmUtils } from "./helpers/TFarm";
 import { Burner } from "./helpers/Burner";
 import { BakerRegistry } from "./helpers/BakerRegistry";
-import { QSFA12Factory } from "./helpers/QSFA12Factory";
-import { QSFA2Factory } from "./helpers/QSFA2Factory";
-import { QSFA12Dex } from "./helpers/QSFA12Dex";
+import { DexCore } from "./helpers/DexCore";
 
 import { WithdrawParams, DepositParams } from "./types/Common";
 import { UpdateOperatorParam } from "./types/FA2";
@@ -19,20 +17,18 @@ import { fa2Storage } from "../storage/test/FA2";
 import { tFarmStorage } from "../storage/TFarm";
 import { burnerStorage } from "../storage/Burner";
 import { bakerRegistryStorage } from "../storage/BakerRegistry";
-import { qsFA12FactoryStorage } from "../storage/test/QSFA12Factory";
-import { qsFA2FactoryStorage } from "../storage/test/QSFA2Factory";
+import { dexCoreStorage } from "storage/test/DexCore";
+import { BigNumber } from "bignumber.js";
 
 describe("TFarm tests (section 4)", async () => {
   var fa12: FA12;
-  var fa12LP: QSFA12Dex;
   var fa2: FA2;
   var qsGov: FA2;
   var utils: Utils;
   var tFarm: TFarm;
   var burner: Burner;
   var bakerRegistry: BakerRegistry;
-  var qsFA12Factory: QSFA12Factory;
-  var qsFA2Factory: QSFA2Factory;
+  var dexCore: DexCore;
 
   var precision = 10 ** 18;
   var feePrecision = 10 ** 16;
@@ -51,62 +47,72 @@ describe("TFarm tests (section 4)", async () => {
       bakerRegistryStorage,
     );
 
-    qsFA12FactoryStorage.baker_validator = bakerRegistry.contract.address;
+    dexCoreStorage.storage.baker_registry = bakerRegistry.contract.address;
+    dexCoreStorage.storage.admin = alice.pkh;
+    dexCoreStorage.storage.collecting_period = new BigNumber(12);
+    dexCore = await DexCore.originate(utils.tezos, dexCoreStorage);
 
-    qsFA12Factory = await QSFA12Factory.originate(
-      utils.tezos,
-      qsFA12FactoryStorage,
-    );
-
-    qsFA2FactoryStorage.baker_validator = bakerRegistry.contract.address;
-
-    qsFA2Factory = await QSFA2Factory.originate(
-      utils.tezos,
-      qsFA2FactoryStorage,
-    );
-
-    await qsFA12Factory.setDexAndTokenLambdas();
-    await qsFA2Factory.setDexAndTokenLambdas();
+    await dexCore.setLambdas();
 
     const updateOperatorParam: UpdateOperatorParam = {
       add_operator: {
         owner: alice.pkh,
-        operator: qsFA2Factory.contract.address,
+        operator: dexCore.contract.address,
         token_id: 0,
       },
     };
 
     await qsGov.updateOperators([updateOperatorParam]);
-    await qsFA2Factory.launchExchange(qsGov.contract.address, 0, 10000, 10000);
-    await qsFA2Factory.updateStorage({
-      token_to_exchange: [[qsGov.contract.address, 0]],
-    });
+    await dexCore.launchExchange(
+      {
+        pair: {
+          token_a: {
+            fa2: { token: qsGov.contract.address, id: 0 },
+          },
+          token_b: { tez: undefined },
+        },
+        token_a_in: new BigNumber(10000),
+        token_b_in: new BigNumber(10000),
+        shares_receiver: alice.pkh,
+        candidate: alice.pkh,
+        deadline: String((await utils.getLastBlockTimestamp()) / 1000 + 100),
+      },
+      10000,
+    );
 
-    const qsGovLPAddress = await qsFA2Factory.storage.token_to_exchange[
-      `${qsGov.contract.address},${0}`
-    ];
+    await dexCore.updateStorage({
+      token_to_id: [[qsGov.contract.address, 0]],
+    });
 
     await fa2.updateOperators([updateOperatorParam]);
-    await qsFA2Factory.launchExchange(fa2.contract.address, 0, 10000, 10000);
+    await dexCore.launchExchange(
+      {
+        pair: {
+          token_a: {
+            fa2: { token: fa2.contract.address, id: 0 },
+          },
+          token_b: { tez: undefined },
+        },
+        token_a_in: new BigNumber(10000),
+        token_b_in: new BigNumber(10000),
+        shares_receiver: alice.pkh,
+        candidate: alice.pkh,
+        deadline: String((await utils.getLastBlockTimestamp()) / 1000 + 100),
+      },
+      10000,
+    );
 
-    await fa12.approve(qsFA12Factory.contract.address, 10000);
-    await qsFA12Factory.launchExchange(fa12.contract.address, 10000, 10000);
-    await qsFA12Factory.updateStorage({
-      token_to_exchange: [fa12.contract.address],
+    await dexCore.updateStorage({
+      token_to_id: [[fa2.contract.address, 1]],
     });
 
-    const fa12LPAddress: string =
-      qsFA12Factory.storage.token_to_exchange[fa12.contract.address];
-
-    fa12LP = await QSFA12Dex.init(fa12LPAddress, utils.tezos);
-
-    burnerStorage.qsgov_lp = qsGovLPAddress;
+    burnerStorage.qsgov_lp = dexCore.contract.address;
     burnerStorage.qsgov.token = qsGov.contract.address;
     burnerStorage.qsgov.id = 0;
 
     tFarmStorage.storage.qsgov.token = qsGov.contract.address;
     tFarmStorage.storage.qsgov.id = 0;
-    tFarmStorage.storage.qsgov_lp = qsGovLPAddress;
+    tFarmStorage.storage.qsgov_lp = dexCore.contract.address;
     tFarmStorage.storage.admin = alice.pkh;
     tFarmStorage.storage.pending_admin = zeroAddress;
     tFarmStorage.storage.burner = zeroAddress;
@@ -126,7 +132,9 @@ describe("TFarm tests (section 4)", async () => {
 
     newFarmParams.fees.harvest_fee = 30 * feePrecision;
     newFarmParams.fees.withdrawal_fee = 6 * feePrecision;
-    newFarmParams.stake_params.staked_token = { fA12: fa12LP.contract.address };
+    newFarmParams.stake_params.staked_token = {
+      fA2: { token: dexCore.contract.address, id: 1 },
+    };
     newFarmParams.stake_params.is_v2_lp = true;
     newFarmParams.reward_per_second = 4 * precision;
     newFarmParams.timelock = 0;
@@ -161,7 +169,15 @@ describe("TFarm tests (section 4)", async () => {
       rewards_receiver: rewardsReceiver,
     };
 
-    await fa12LP.approve(tFarm.contract.address, depositParams.amt);
+    await dexCore.updateOperators([
+      {
+        add_operator: {
+          owner: alice.pkh,
+          operator: tFarm.contract.address,
+          token_id: 1,
+        },
+      },
+    ]);
     await tFarm.deposit(depositParams);
     await tFarm.withdraw(withdrawParams);
   });

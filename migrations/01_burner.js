081510c4aadd7063c0b28e8241ce746252867057
cyace84 <cyace84@gmail.com>
2022-08-23 12:54:30 +0200

    fix migration

--- migrations/01_burner.js
+++ migrations/01_burner.js
@@ -7,6 +7,7 @@ const env = require("../env");
 module.exports = async (tezos, network) => {
   burnerStorage.qsgov = env.networks[network].qsgov;
   burnerStorage.qsgov_lp = env.networks[network].qsgov_lp;
+  burnerStorage.pool_id = env.networks[network].qsgov_lp_id;
 
   const burnerAddress = await migrate(tezos, "burner", burnerStorage, network);
 
